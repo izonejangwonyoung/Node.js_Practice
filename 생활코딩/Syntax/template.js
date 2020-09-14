324035e07719b951de63cc637785c4fef32aee09
izonejangwonyoung <2000shim@naver.com>
2020-09-14 22:51:10 +0900

    템플릿 연습

---
+++ 생활코딩/Syntax/template.js
@@ -0,0 +1,12 @@
+var letter= 'WE ARE izone EYES ON ME! WIZONE!!'
+var name='IZONE'
+console.log(letter)
+
+//변수 기능 활용
+
+///오류나는 이유 모르겠다
+
+var letter= `WE ${name} ARE izone EYES ON ME! WIZONE!!`;
+
+
+
