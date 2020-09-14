6357e365e8bbb9d8a40097d12302fa7d43bb5499
izonejangwonyoung <2000shim@naver.com>
2020-09-15 00:32:13 +0900

    App - 홈페이지 구현

--- 생활코딩/localhost3000/main.js
+++ 생활코딩/localhost3000/main.js
@@ -5,40 +5,68 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    fs.readFile(`d:/NodeJsFolder/Node.js_Practice/생활코딩/data/${queryData.id}`,'utf8',function(err,description){
+    var pathname = url.parse(_url, true).pathname;
+ 
+    if(pathname === '/'){
+
+      if(queryData.id === undefined){
+       
+      
+      fs.readFile(`d:/NodeJsFolder/Node.js_Practice/생활코딩/data/${queryData.id}`, 'utf8', function(err, description){
+        var title = 'Welcome';
+        var description  = 'Hello, Node.js';
         
         var template = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ul>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ul>
-      <h2>${title}</h2>
-      <p>${description}
-      </p>
-    </body>
-    </html>
-    `;   response.end(template);
-
-    })
-    
-  
- 
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else {
+      fs.readFile(`d:/NodeJsFolder/Node.js_Practice/생활코딩/data/${queryData.id}`, 'utf8', function(err, description){
+        var title = queryData.id;
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    }
+    } else {
+    response.writeHead(404);
+    response.end('Not found');
+  }
 });
 app.listen(3000);
