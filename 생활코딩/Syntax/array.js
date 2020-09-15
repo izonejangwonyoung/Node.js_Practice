29b296538870739d4f94383dfa9cf6491bafaa5d
izonejangwonyoung <2000shim@naver.com>
2020-09-15 10:54:47 +0900

    배열

---
+++ 생활코딩/Syntax/array.js
@@ -0,0 +1,11 @@
+//CRUD
+
+var arr= ['장원영','김채원','송하영','김민주','이채영'];
+console.log(arr[3]);
+console.log(arr[2]);
+arr[2]='박지원';
+console.log(arr);
+console.log(arr.length);
+arr.push('백지헌')
+console.log(arr);
+console.log(arr.length);
