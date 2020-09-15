2f1973c36a2952901a93ca529bee5df01f180ef2
izonejangwonyoung <2000shim@naver.com>
2020-09-15 21:50:08 +0900

    파일리딩 구현

--- LIFECODE/localhost3000/main.js
+++ LIFECODE/localhost3000/main.js
@@ -6,67 +6,79 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
- 
     if(pathname === '/'){
-
       if(queryData.id === undefined){
-       
-      
-      fs.readFile(`d:/NodeJsFolder/Node.js_Practice/생활코딩/data/${queryData.id}`, 'utf8', function(err, description){
-        var title = 'Welcome';
-        var description  = 'Hello, Node.js';
-        
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
-      });
+ 
+        fs.readdir('D:/NodeJsFolder/Node.js_Practice/LIFECODE/data', function(error, filelist){
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var list = '<ul>';
+          var i = 0;
+          while(i < filelist.length){
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i = i + 1;
+          }
+          list = list+'</ul>';
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        })
+ 
+ 
+ 
+      } else {
+        fs.readdir('D:/NodeJsFolder/Node.js_Practice/LIFECODE/data', function(error, filelist){
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var list = '<ul>';
+          var i = 0;
+          while(i < filelist.length){
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i = i + 1;
+          }
+          list = list+'</ul>';
+          fs.readFile(`D:/NodeJsFolder/Node.js_Practice/LIFECODE/data/${queryData.id}`, 'utf8', function(err, description){
+            var title = queryData.id;
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+          });
+        });
+      }
     } else {
-      fs.readFile(`d:/NodeJsFolder/Node.js_Practice/생활코딩/data/${queryData.id}`, 'utf8', function(err, description){
-        var title = queryData.id;
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
-      });
+      response.writeHead(404);
+      response.end('Not found');
     }
-    } else {
-    response.writeHead(404);
-    response.end('Not found');
-  }
+ 
+ 
+ 
 });
 app.listen(3000);
