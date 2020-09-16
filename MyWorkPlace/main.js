b86f12b1a16a2d3658ce26d2a5df3bd99c2baae7
izonejangwonyoung <2000shim@naver.com>
2020-09-16 13:14:14 +0900

    my workplace

---
+++ MyWorkPlace/main.js
@@ -0,0 +1,70 @@
+const  http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+function templateHTML(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+function templateList(filelist){
+
+  var list = '<ul>';
+  var i = 0;
+  while(i < filelist.length){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list+'</ul>';
+  return list;
+}
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    if(pathname === '/'){
+      if(queryData.id === undefined){
+ 
+        fs.readdir('d:/NodeJsFolder/Node.js_Practice/MyWorkPlace/data/', function(error, filelist){
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+          response.writeHead(200);
+          response.end(template);
+        })
+ 
+ 
+ 
+      } else {
+        fs.readdir('d:/NodeJsFolder/Node.js_Practice/MyWorkPlace/data/', function(error, filelist){
+          
+          fs.readFile(`d:/NodeJsFolder/Node.js_Practice/MyWorkPlace/data/${queryData.id}`, 'utf8', function(err, description){
+            var title = queryData.id;
+            var list = templateList(filelist);
+            var template = templateHTML(title, list,`<h2>${title}</h2>${description}`);
+            response.writeHead(200);
+            response.end(template);
+          });
+        });
+      }
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
+    }
+ 
+ 
+ 
+});
+app.listen(3000);
--- 생활코딩/filreread_practice/readdir.js
+++
@@ -1,6 +0,0 @@
-var testFolder = 'D:\NodeJsFolder\Node.js_Practice\LIFECODE\data';
-var fs = require('fs');
- 
-fs.readdir(testFolder, function(error, filelist){
-  console.log(filelist);
-})
