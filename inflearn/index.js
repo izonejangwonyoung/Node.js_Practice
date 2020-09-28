e72c2a70814e3591925f1989ba0f76e67627c737
izonejangwonyoung <2000shim@naver.com>
2020-09-28 20:00:18 +0900

    #12 토큰 생성

--- inflearn/index.js
+++ inflearn/index.js
@@ -2,14 +2,17 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser=require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const config=require('./config/key')
 const { User } = require("./models/user");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
-const mongoose= require('mongoose')
+const mongoose= require('mongoose');
+
 
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify: false
@@ -39,6 +42,50 @@ app.post('/register',(req, res)=> {
     })
   })
 })
+
+
+
+
+
+
+app.post('./login',(req,res)=> {
+
+
+  User.findOne({email: req.body.email}, (err, user)=> {
+    if(!user){
+      return res.json({
+        loginSuccess: false,
+        message:"제공된 이메일에 해당하는 유저가 없습니다."
+
+      })
+    }
+
+    user.comparePassword(req.body.password , (err,isMatch)=>{
+      if(!isMatch)
+      return res.json({loginSuccessL: false,message:"비밀번호가 틀렸습니다."})
+
+
+      user.generateToken((err,user)=>{
+
+        if(err) return res.status(400).send(err);
+
+        //토큰을 저장한다. 어디에? 쿠키? local storage?
+        res.cookie("x_auth",user.token)
+        .status(200)
+        .json({loginSuccess: true, userId:user._id})
+
+
+      })
+
+
+    })
+  })
+
+
+})
+
+
+
 
 
 
--- inflearn/models/user.js
+++ inflearn/models/user.js
@@ -1,5 +1,7 @@
 const mongoose= require('mongoose');
-
+const bcrypt= require('bcrypt');
+const saltRounds=10;
+const jwt=require('jsonwebtoken')
 
 const userSchema=mongoose.Schema({
     name: {
@@ -9,7 +11,7 @@ const userSchema=mongoose.Schema({
     email: {
         type:String,
         trim:true,
-        
+        unique:1
     },
     password: {
         type: String,
@@ -38,6 +40,65 @@ const userSchema=mongoose.Schema({
 
 })
 
+
+userSchema.pre('save',function(next){
+    var user = this;
+
+    if(user.isModified('password')){
+    bcrypt.genSalt(saltRounds,function(err,salt){
+
+        if (err) return next(err)
+        bcrypt.hash(user.password , salt , function(err,hash){
+        
+            if(err )return next(err)
+        user.password=hash
+        next()
+
+            
+        })
+
+    })
+    }   else{
+        next()
+    }
+})
+
+userSchema.methods.comparePassword=function(plainPassword,cb){
+
+
+    bcrypt.compare(plainPassword, this.password, function(err,isMatch){
+
+        if (err) return cb(err),
+        cb(null,isMatch)
+    })
+}
+
+
+userSchema.methods.generateToken=function(cb){
+
+    var user=this;
+
+    var token = jwt.sign(user._id.toHexString, 'secretToken')
+    
+
+    user.token=token
+    user.save(function(err,user){
+
+
+        if (err) return cb(err)
+        cb(null , user )
+    })
+
+
+
+
+
+
+
+
+
+
+}
 
 const User=mongoose.model('User',userSchema)
 
