2965bf63b93431633effe2f33de11ca988540648
izonejangwonyoung <2000shim@naver.com>
2020-11-15 22:36:02 +0900

    __dirname

--- MyWorkPlace_textbook/chapter3/async.js
+++ MyWorkPlace_textbook/chapter3/async.js
@@ -1,20 +1,20 @@
-const fs=require('fs');
+const fs=require('fs')
 
 console.log('시작');
-fs.readFile('./readme2.txt',(err,data)=>{
+fs.readFile(__dirname+'/readme2.txt',(err,data)=>{
     if (err){
         throw err;
     }
     console.log('1번',data.toString());
 });
-fs.readFile('./readme2.txt',(err,data)=>{
+fs.readFile(__dirname+'/readme2.txt',(err,data)=>{
     if (err) {
         throw err;
 
     }
     console.log('2번',data.toString());
 });
-fs.readFile('./readme2.txt',(err,data)=>{
+fs.readFile(__dirname+'/readme2.txt',(err,data)=>{
     if (err) {
         throw err;
     }
