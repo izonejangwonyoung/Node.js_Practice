2db964a5bf812a6cfc6eb7d58c1905c4ef184d71
izonejangwonyoung <wh0015~~!!@@>
2020-11-15 02:29:09 -0500

    test notebook TG

--- MyWorkPlace_textbook/chapter3/asyncOrderPromise.js
+++ MyWorkPlace_textbook/chapter3/asyncOrderPromise.js
@@ -1,4 +1,19 @@
 const fs =require('fs').promises;
 
 console.log('시작');
-fs.readFile()
+fs.readFile('./readme2.txt')
+.then((data)=> {
+    console.log('number1',data.toString());
+    return fs.readFile('./readme2.txt');
+})
+.then((data)=> {
+    console.log('number2',data.toString());
+    return fs.readFile('./readme2.txt');
+})
+.then((data)=> {
+    console.log('number3',data.toString());
+console.log('END');
+})
+.catch((err)=>{
+    console.err(err);
+});
